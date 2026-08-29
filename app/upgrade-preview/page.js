8263af971646aee81b2d4e529f62e3d3b2f83c81
manzoornel <139558118+manzoornel@users.noreply.github.com>
2026-08-29 16:27:49 +0530

    Add UpgradePreviewPage component with metadata

---
+++ app/upgrade-preview/page.js
@@ -0,0 +1,8 @@
+import UpgradePreview from "@/components/UpgradePreview";
+
+export const metadata = { title: "BrandMD Operations Upgrade Preview" };
+
+export default function UpgradePreviewPage() {
+  return <UpgradePreview />;
+}
+
